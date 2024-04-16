4bbb91c9f5574a7a7e1bc7f8c5239f5138f8ecee
Kondreddi Ajay Kumar <96619578+ajaykumar703@users.noreply.github.com>
2024-04-16 10:37:36 +0530

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "/smartportfolio",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
